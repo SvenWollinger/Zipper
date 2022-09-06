35e77921f028bcfe2c5fa8e6433588ad15782be9
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-09-06 20:03:47 +0200

    Adjust gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "io.wollinger"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.json:json:20220320")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes["Main-Class"] = "io.wollinger.zipper.MainKt"
+    }
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        sourceSets.main.get().output
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
